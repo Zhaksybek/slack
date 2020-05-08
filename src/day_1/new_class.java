955be4e282467134a0fbf3e52390f0b8c79a12cb
mr.jackson02 <62046646+Zhaksybek@users.noreply.github.com>
2020-05-08 12:11:09 -0700

    Update new_class.java

--- src/day_1/new_class.java
+++ src/day_1/new_class.java
@@ -3,5 +3,7 @@ package day_1;
 public class new_class {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        
+        // Hey, I just wanna add some commit! good luck!
     }
 }
